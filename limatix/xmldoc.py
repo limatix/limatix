Fix unit conversion bug in xmldoc.xpathnumpy; add beginnings of an xmldoc api tutorial

limatix/xmldoc.py
@@ -2043,7 +2043,7 @@ class xmldoc(object):
         """Alias for xpath(path,namespaces,contextnode)"""
         return self.xpath(path,namespaces=namespaces,contextnode=contextnode,extensions=extensions,variables=variables,noprovenance=noprovenance)
         
-    def xpathnumpy(self,path,namespaces=None,contextnodes=None,extensions=None,variables=None,iscomplex=False,oneper=True):
+    def xpathnumpy(self,path,namespaces=None,contextnodes=None,extensions=None,variables=None,iscomplex=False,oneper=True,desiredunits=None):
         """Like xpath, but convert node-set to floating point, return as 
         numpy array along with units.
 
@@ -2080,7 +2080,17 @@ class xmldoc(object):
         # nodeset=[]
         
         resultarray=np.zeros(len(nodeset),dtype=dtype)
-        resultunits=None
+
+        if desiredunits is not None:
+            if isinstance(desiredunits,basestring):
+                desiredunits=lmu.parseunits(desiredunits)
+                pass
+            resultunits=desiredunits
+            pass
+        else:
+            resultunits=None
+            pass
+        
         if len(nodeset) > 0:
 
             for cnt in range(len(nodeset)):
@@ -2135,16 +2145,16 @@ class xmldoc(object):
 
 
                 
-                resultarray[cnt]=coeff*converted
+                resultarray[cnt]=converted/coeff
                                 
                 pass
             pass
         return (resultarray,resultunits)
 
 
-    def xpathcontextnumpy(self,contextnodes,path,namespaces=None,extensions=None,variables=None,iscomplex=False,oneper=True):
+    def xpathcontextnumpy(self,contextnodes,path,namespaces=None,extensions=None,variables=None,iscomplex=False,oneper=True,desiredunits=None):
         """Like xpathnumpy but must provide context nodes"""
-        return self.xpathnumpy(path,namespaces=namespaces,contextnodes=contextnodes,extensions=extensions,variables=variables,iscomplex=iscomplex,oneper=oneper)
+        return self.xpathnumpy(path,namespaces=namespaces,contextnodes=contextnodes,extensions=extensions,variables=variables,iscomplex=iscomplex,oneper=oneper,desiredunits=desiredunits)
 
     def xpathtonodestrlist(self,path,namespaces=None,contextnodes=None,extensions=None,variables=None,oneper=True):
         """Intended for internal use only. Convert an xpath and provided 