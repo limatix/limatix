263dc37339ed07e22aea78e26a8f9b2408abe855
Steve Holland <sdh4@iastate.edu>
2026-01-23 12:24:09 -0600

    lm_units: Improve elimination in the unitless case

--- limatix/lm_units.py
+++ limatix/lm_units.py
@@ -423,7 +423,11 @@ class units(object):
 
         pos=0
         while pos < len(self.Factors)-1:
-            if self.Factors[pos]==self.Factors[pos+1]:
+            if self.Factors[pos].Unit is None:
+                # omit unitless
+                del self.Factors[pos]
+                pass
+            elif self.Factors[pos]==self.Factors[pos+1]:
                 self.Factors[pos].Power += self.Factors[pos+1].Power
                 del self.Factors[pos+1]
 
@@ -717,7 +721,7 @@ def copyunits(comb):
 def comparerawunits(CombA,CombB):
     # returns 0 for non-equal, When the unit combinations are equivalent, the coefficient of CombA relative
     #  to CombB is returned. 
-    # CombA and CombB MUST be already sorted sith wortunits() method
+    # CombA and CombB MUST be already sorted with sortunits() method
 
     if len(CombA.Factors) != len(CombB.Factors):
         return 0.0
